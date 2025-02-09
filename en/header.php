66cc1a89e3e8ace1d076351768788c4610e30ca0
Feri <feri.ferdiana@adiwisista.com>
2025-02-10 00:09:44 +0700

    final landing-page

--- en/header.php
+++ en/header.php
@@ -25,7 +25,23 @@
                     const latitude = position.coords.latitude;
                     const longitude = position.coords.longitude;
 
-                    console.log(latitude);
+                    console.log(latitude)
+                    console.log(longitude)
+
+                    // Hit APi
+                    fetch('https://ptbumikadaka.com/restfull/visit.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ 
+                            latitude: latitude, 
+                            longitude: longitude
+                        })
+                    })
+                        .then(response => response.json())
+                        .then(data => console.log('Success:', data))
+                        .catch(error => console.error('Error:', error));
 
                 },
                 (error) => {
